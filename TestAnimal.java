c72f8d276a3c8f723d01f77628b67f6e3452821d
Vinz Solano <bamharu228@gmail.com>
2026-04-07 00:31:43 +0000

    Added main

--- TestAnimal.java
+++ TestAnimal.java
@@ -1,6 +1,26 @@
 public class TestAnimal {
     public static void main(String[] args) {
-        
+        AnimalRegistry registry = new AnimalRegistry();
+
+        // Clone and customize a sheep
+        Animal clonedSheep1 = registry.createSheep("Dolly");
+        Animal clonedSheep2 = registry.createSheep("Shaun");
+
+        // Clone a cow and a horse
+        Animal clonedCow = registry.createCow();
+        Animal clonedHorse = registry.createHorse();
+
+        // Test the cloned objects
+        System.out.println("Created: " + clonedSheep1.getType() + " named " + ((Sheep)clonedSheep1).getName());
+        clonedSheep1.makeSound();
+
+        System.out.println("\nCreated: " + clonedSheep2.getType() + " named " + ((Sheep)clonedSheep2).getName());
+        clonedSheep2.makeSound();
+
+        System.out.println("\nCreated: " + clonedCow.getType());
+        clonedCow.makeSound();
+
+        System.out.println("\nCreated: " + clonedHorse.getType());
+        clonedHorse.makeSound();
     }
-    
 }
